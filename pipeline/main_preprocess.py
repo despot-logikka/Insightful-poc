7009bc394d1309e4a44245bcfc4c6ad3bd304e44
despotmarkovic <despotmarkovic@gmail.com>
2024-12-07 19:55:55 +0100

    updated main_preprocess.py

--- pipeline/main_preprocess.py
+++ pipeline/main_preprocess.py
@@ -1,8 +1,19 @@
+import os
 import yaml
 from utils import load_and_process_csv_files
 
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(current_dir)
+
+# Build the full path to the config file
+config_path = os.path.join(current_dir, 'configs', 'preprocessing_config.yaml')
+
+print(config_path)
+
 # Load configuration from 'preprocessing.yaml'
-with open('configs/preprocessing_config.yaml', 'r') as config_file:
+with open(config_path, 'r') as config_file:
     config = yaml.safe_load(config_file)
 
 # Define the input directory and output path for combined CSV from the config file
