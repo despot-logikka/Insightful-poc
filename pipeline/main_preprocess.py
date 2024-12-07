a7b926b5a280b7d8804c6f3b43744e0a46e737b8
despotmarkovic <despotmarkovic@gmail.com>
2024-12-07 21:44:53 +0100

    added bash

--- pipeline/main_preprocess.py
+++ pipeline/main_preprocess.py
@@ -16,14 +16,15 @@ try:
     print(os.listdir('/'))
     print(os.listdir('/data/'))
     print(os.listdir('/exp/'))
+    print(os.listdir('/out/'))
 except Exception as e:
     print(e)
 
-input_dir = config['paths']['input_dir']
-output_path = config['paths']['output_path']
+# input_dir = config['paths']['input_dir']
+# output_path = config['paths']['output_path']
 
-print(f"Input directory: {input_dir}")
-print(f"Output path: {output_path}")
+# print(f"Input directory: {input_dir}")
+# print(f"Output path: {output_path}")
 
-# Combine and save CSV files
-load_and_process_csv_files(input_dir, output_path)
+# # Combine and save CSV files
+# load_and_process_csv_files(input_dir, output_path)
